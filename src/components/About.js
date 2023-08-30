5746584345adb2ffbb5d2b6e0cb60d32252d1917
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-08-31 00:28:19 +0530

    logo change, about and topics completed

--- src/components/About.js
+++ src/components/About.js
@@ -10,14 +10,46 @@ export default function About() {
       }} className="p-6 bg-[#020411] text-gray-300">
         
         <h1 className='text-3xl font-bold'>About Conference</h1>
-        <p className='text-md md:text-xl py-10 text-justify'>2nd International Conference on recent advances in aerospace engineering aims to bring together leading academic scientists, researchers and research scholars to exchange and share their experiences and research results on all aspects of Aerospace Sciences. It also provides a premier interdisciplinary platform for researchers, practitioners and educators to present and discuss the most recent innovations, trends, and concerns as well as practical challenges encountered and solutions adopted in the fields of Aerospace.</p>
+        <p className='text-md md:text-xl py-6 text-justify'>2nd International Conference on recent advances in aerospace engineering aims to bring together leading academic scientists, researchers and research scholars to exchange and share their experiences and research results on all aspects of Aerospace Sciences. It also provides a premier interdisciplinary platform for researchers, practitioners and educators to present and discuss the most recent innovations, trends, and concerns as well as practical challenges encountered and solutions adopted in the fields of Aerospace.</p>
 
         <h1 className='text-3xl font-bold'>
-        Objectives of Conference
+        Objectives of the Conference
         </h1>
-        <p className="text-md md:text-xl py-10 text-justify">
+        <p className="text-md md:text-xl py-6 text-justify">
         Prospective authors are kindly encouraged to contribute to and help shape the conference through submissions of their research abstracts and papers. Also, high quality research contributions describing original and unpublished results of conceptual, constructive, empirical, experimental, or theoretical work in all areas of Aerospace are cordially invited for presentation at the conference.
         </p>
+
+        <h1 className="text-3xl font-bold">Call for Papers</h1>
+        <p className='text-md md:text-xl py-6 text-justify'>
+        Prospective authors are kindly encouraged to contribute to and help shape the conference through submissions of their research abstracts and papers. Also, high quality research contributions describing original and unpublished results of conceptual, constructive, empirical, experimental, or theoretical work in all areas of Aerospace are cordially invited for presentation at the conference.
+        </p>
+        <h1 className='text-3xl font-bold pb-6 '>Important Dates</h1>
+        <table className='border-spacing-5 border-2 border-slate-500'>
+          <tr className=''>
+            <td className='p-5 border border-slate-700'>Abstract Submission Deadline</td>
+            <td className='p-5 border border-slate-700'>2 <sup>nd</sup> Oct 2023</td>
+          </tr>
+          <tr>
+            <td className='p-5 border border-slate-700'>Full Paper Submission Deadline</td>
+            <td className='p-5 border border-slate-700'>16 <sup>th</sup> Oct 2023</td>
+          </tr>
+          <tr>
+            <td className='p-5 border border-slate-700'>Notification of Acceptance</td>
+            <td className='p-5 border border-slate-700'>16 <sup>th</sup> Oct 2023</td>
+          </tr>
+          <tr>
+            <td className='p-5 border border-slate-700'>Last Date of Registration & Payment</td>
+            <td className='p-5 border border-slate-700'>16 <sup>th</sup> Oct 2023</td>
+          </tr>
+          <tr>
+            <td className='p-5 border border-slate-700'>Last Date For Revised Paper Submission</td>
+            <td className='p-5 border border-slate-700'>16 <sup>th</sup> Oct 2023</td>
+          </tr>
+          <tr>
+            <td className='p-5 border border-slate-700'>Conference Dates</td>
+            <td className='p-5 border border-slate-700'>29 <sup>th</sup> & 30 <sup>th</sup> March 2024</td>
+          </tr>
+        </table>
 
       </div>
   )
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,22 +1,121 @@
-import React from 'react'
+import React from "react";
 
 export default function Topics() {
   return (
-    <div className=''>
-        <div
+    <div className="">
+      <div
         style={{
-            backgroundImage: "url(/stars.jpg)",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            height: "100%",
-          }} className='text-gray-300' >
-            <div>
-            <h1 className='text-2xl '>Topics</h1>
-            <div className='w-56 h-56 border-2 border-solid border-white rounded-xl'>
-                <h1 className='text-center'>Propulsion</h1>
-            </div>
-            </div>
+          backgroundImage: "url(/stars.jpg)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100%",
+        }}
+        className="p-6 text-gray-300"
+      >
+        <div className="flex justify-center items-center">
+          <table className="border-spacing-5 border-2 border-slate-500 m-5 w-1/2">
+            <tr className="">
+              <td className="p-5 border border-slate-700">1</td>
+              <td className="p-5 border border-slate-700">Propulsion</td>
+              <td className="p-5 border border-slate-700">
+                <ul className="p-5 list-decimal space-y-3">
+                  <li>Advanced In-Space Propulsion & Power Concepts</li>
+                  <li>Advances in Space Propulsion</li>
+                  <li>Propulsion Analysis</li>
+                  <li>Clean Combustion</li>
+                  <li>Green propellant</li>
+                  <li>Fluid Mechanics</li>
+                </ul>
+              </td>
+            </tr>
+            <tr className="">
+              <td className="p-5 border border-slate-700">2</td>
+              <td className="p-5 border border-slate-700">Structures</td>
+              <td className="p-5 border border-slate-700">
+                <ul className=" p-5 list-decimal space-y-3">
+                  <li>Materials</li>
+                  <li>Aviation 4.0</li>
+                  <li>Heat Transfer</li>
+                  <li>Theories, Models and Concepts</li>
+                  <li>Advanced Energy Sources</li>
+                  <li>Advanced Heat Pipe Technologies</li>
+                  <li>Thermal Control</li>
+                  <li>Thermal Control for Lunar and Deep Missions</li>
+                  <li>Two Phase Thermal Control Systems</li>
+                  <li>Advanced Thermal Control Technologies</li>
+                </ul>
+              </td>
+            </tr>
+            <tr className="">
+              <td className="p-5 border border-slate-700">3</td>
+              <td className="p-5 border border-slate-700">Space Dynamics</td>
+              <td className="p-5 border border-slate-700">
+                <ul className=" p-5 list-decimal space-y-3">
+                  <li>Lunar in Space Resource Utilization</li>
+                  <li>Lunar Lander Technologies and Design</li>
+                  <li>Avionics</li>
+                  <li>Astrophysics</li>
+                  <li>New Directions in Astrophysics/Particle Physics</li>
+                  <li>Enabling Technologies for Lunar Surface Science</li>
+                  <li>
+                    Far Term Space Transport and Environment Models and Theories
+                  </li>
+                  <li>High Capacity Heat Rejection Systems</li>
+                  <li>Innovative Techniques in Nuclear Energy</li>
+                  <li>Microgravity Thermophysics</li>
+                  <li>New Directions in Communications</li>
+                  <li>Planetary Defense and Societal Protection</li>
+                  <li>Sensors</li>
+                  <li>Smart Materials</li>
+                  <li>Space faring societies</li>
+                  <li>Space policy and space law in a social context</li>
+                  <li>Space Science and Technology Roadmaps</li>
+                  <li>Space societies/ the settlement of space environments</li>
+                  <li>Technology transfers and spinoffs</li>
+                  <li>
+                    The relationship between astrosociology and astrobiology
+                  </li>
+                  <li>Theories on High Frequency Gravity Wave</li>
+                  <li>
+                    Unconventional Physical Principles and Gravitational Models
+                  </li>
+                  <li>
+                    Engineering Design including Vibration & Noise, Thermal
+                    Engineering, Cryogenics, Heat Transfer
+                  </li>
+                  <li>
+                    Non-conventional & Renewable Energy, Fluid Mechanics &
+                    Machinery
+                  </li>
+                  <li>
+                    Optimisation, Simulation & Modelling, Dynamic Systems,
+                    Measurement & Control
+                  </li>
+                  <li>
+                    Reliability, Availability & Maintainability, Tribology
+                    including Bearings
+                  </li>
+                  <li>
+                    Artificial Intelligence, Knowledge Intensive CAD/CAM,
+                    Computer Aided Product Development, PDM / Enterprise
+                    Information Management, Computing Technologies
+                  </li>
+                  <li>
+                    Bioengineering, Perception Based Engineering, Smart
+                    Technology, Mechatronics, Micro Electronics in Mechanical
+                    System
+                  </li>
+                  <li>
+                    Energy Efficiency & Energy Conservation, Power Plant
+                    Engineering
+                  </li>
+                  <li>Automation, Control & Robotics, Casting & Welding</li>
+                </ul>
+              </td>
+            </tr>
+          </table>
         </div>
+      </div>
     </div>
-  )
+  );
 }
