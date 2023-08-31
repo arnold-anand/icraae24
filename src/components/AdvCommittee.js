12bbde5fed4397f4d9fd1285864b18c3657da8ae
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-08-31 22:18:46 +0530

    Advisory committee complete

--- src/components/AdvCommittee.js
+++ src/components/AdvCommittee.js
@@ -11,10 +11,10 @@ export default function AdvCommittee() {
       }}
       className=" flex flex-col justify-center items-center p-6 bg-[#020411] text-gray-300"
     >
-      <h1 className=" sticky top-5 font-bold text-xl text-left my-5">
+      <h1 className="font-bold text-xl text-left my-5">
         International Advisory Committee
       </h1>
-      <table className="md:w-1/2">
+      <table className=" w-screen text-xs md:w-1/2 md:text-md">
         <tr className="text-left">
           <th className="p-2">Name</th>
           <th className="p-2">Designation</th>
@@ -93,7 +93,7 @@ export default function AdvCommittee() {
         </tr>
         <tr>
           <td className="p-2 border-b border-slate-500 text-left">
-            Dr.Joseph Lefkowitz{" "}
+            Dr.Joseph Lefkowitz
           </td>
           <td className="p-2 border-b border-slate-500 text-left">
             Assistant Professor
@@ -117,11 +117,11 @@ export default function AdvCommittee() {
       <h1 className="font-bold text-xl text-left my-5">
         National Advisory Committee
       </h1>
-      <table className="md:w-1/2">
+      <table className="w-screen md:w-1/2 text-xs md:text-md">
         <tr>
-          <th className="p-2">Name</th>
-          <th className="p-2">Designation </th>
-          <th className="p-2">Institute</th>
+          <th className="p-2 text-left">Name</th>
+          <th className="p-2 text-left">Designation </th>
+          <th className="p-2 text-left">Institute</th>
         </tr>
         <tr>
           <td className="p-2 border-b border-slate-500 text-left">
@@ -134,9 +134,144 @@ export default function AdvCommittee() {
         </tr>
         <tr>
           <td className="p-2 border-b border-slate-500">Dr. T Jayachandran</td>
-          <td className="p-2 border-b border-slate-500">Retired Deputy</td>
-          Director, VSSC/ISRO & Current visiting Professor at IITM
+          <td className="p-2 border-b border-slate-500">
+            Retired Deputy Director, VSSC/ISRO & Current visiting Professor at
+            IITM
+          </td>
           <td className="p-2 border-b border-slate-500">IIT-Madras</td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. R C Mehta
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Former Head, Aerodynamics Division, VSSC/ISRO, current Dean School
+            of Aeronautical studies.
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Nooral Islam, Nagercoil, India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. Mahesh
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Associate Professor
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Department of Mechanical &amp; Aerospace Engineering, IIT Hyderabad,
+            India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr.Arnalt Stalin
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Senior Manager -EDC-AQCS
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            BHEL, Ranipet , India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr.Benny Karunakar
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Associate Professor ,Dept of Mechanical &amp; Industrial Engineering
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            IIT , Roorkee , India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. Kuldeep Kr. Saxena
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Head, Department of Research Impact and Outcome (LFTS) ,Division of
+            Research and Development
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Lovely Professional University, Phagwara, Jalandhar, India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. G.L.Samuel
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Professor, Department of Mechanical Engineering,IIT Madras
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            IIT Madras, India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">Dr. Sunil</td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Deputy Director
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            LPSC, ISRO, India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. John Tharakan
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Group Director
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            LPSC, ISRO, India
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Mr. Gerald
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Senior Manager
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Wind care-India Pvt Ltd
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. R Santhanakrishnan
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Director Academic-
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            SAARC Maststech Pvt Ltd, Karnataka
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Dr. Srisha Rao M V
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Associate Professor
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Aerospace dept, IISc
+          </td>
+        </tr>
+        <tr>
+          <td className="p-2 border-b border-slate-500 text-left">
+            R Perumal pillai
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Scientist F
+          </td>
+          <td className="p-2 border-b border-slate-500 text-left">
+            Deputy Division Head,SAIE,IPRC
+          </td>
         </tr>
       </table>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     // bg-[#020411]
     <footer className="text-gray-600 body-font bg-[#020411]">
-  <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-200">
-      <span className="ml-3 text-xl"> <img className="w-20" src="karunya-logo-white.png" alt="" />ICRAAE - 2024</span>
-    </a>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Division of Aerospace <br />
-      <a className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">Developed by Karunya Hacks</a>
-    </p>
-  </div>
-</footer>
-  )
+      <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-200">
+          <span className="ml-3 text-xl">
+            {" "}
+            <img className="w-20" src="karunya-logo-white.png" alt="" />
+            ICRAAE - 2024
+          </span>
+        </a>
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          © 2023 Division of Aerospace <br />
+          <a
+            className="text-gray-600 ml-1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Developed by Karunya Hacks
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
