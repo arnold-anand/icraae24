78bbb378908bb8d4951d3d56490f1b5238b409c4
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-09-02 21:20:23 +0530

    Footer correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,11 +14,12 @@ export default function Footer() {
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2023 Division of Aerospace <br />
           <a
+            href="https://instagram.com/karunyahacks?igshid=NjIwNzIyMDk2Mg=="
             className="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
           >
-            Developed by Karunya Hacks
+            Developed by Arnold @ KarunyaHacks
           </a>
         </p>
       </div>
