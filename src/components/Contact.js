a5e1d3d339c398f5da168c727321d184b1e14431
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-09-10 20:22:50 +0530

    images removed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,11 +12,6 @@ export default function Contact() {
           <div className="flex flex-wrap -m-2">
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-              <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/88x88"
-                />
                 <div className="flex-grow">
                     
                   <h2 className="text-[#222] title-font font-medium">
@@ -32,11 +27,6 @@ export default function Contact() {
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-              <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/88x88"
-                />
                 <div className="flex-grow">
                   <h2 className="text-[#222] title-font font-medium">
                   Dr. S. Venkatachalam
@@ -48,11 +38,6 @@ export default function Contact() {
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-                <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/88x88"
-                />
                 <div className="flex-grow">
                   <h2 className="text-[#222] title-font font-medium">
                   Dr. K. Anton Savio Lewise
