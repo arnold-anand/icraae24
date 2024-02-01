152d1e14213d9dbc4e7b8f825c2d1f372ac7166a
arnold-anand <pagadalaarnold@karunya.edu.in>
2024-02-01 11:03:06 +0530

    mobile view landing updated

--- src/components/Mobilelanding.js
+++ src/components/Mobilelanding.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function () {
   return (
     <div>
-        <img className='h-100' src="icraae24-banner-compressed.png" alt="" />
+        <img className='h-100' src="icraae24-banner-compressed.jpg" alt="" />
     </div>
   )
 }
